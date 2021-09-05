33c8d9438231eeddd6b7356c272a6ebab55cf8f0
likhithchatiri1 <likhith.chatiri@terralogic.com>
2021-09-05 13:19:58 +0530

    Physicalmemory code added

--- Physical_Memory.py
+++ Physical_Memory.py
@@ -8,7 +8,7 @@ def get_ram_usage():
     print('RAM memory', psutil.virtual_memory())
     print('RAM memory total:', psutil.virtual_memory().total)
     print('RAM memory used:', psutil.virtual_memory().used,",",'RAM memory % used:', psutil.virtual_memory()[2])
-    print('RAM memory available:', psutil.virtual_memory().available)
+    print('RAM memory available:', psutil.virtual_memory().available,",",'RAM memory % used:',100 - psutil.virtual_memory()[2])
 
 get_cpu_usage_per()
 get_ram_usage()
