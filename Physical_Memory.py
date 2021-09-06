45f4d02accad190a12aebf0e1e444a5191afed9b
likhithchatiri1 <likhith.chatiri@terralogic.com>
2021-09-06 12:45:18 +0530

    NIC code updated

--- Physical_Memory.py
+++ Physical_Memory.py
@@ -1,14 +1,16 @@
 import psutil
+import platform
+# def get_cpu_usage_per():
+#
+#     print("CPU usage %",psutil.cpu_percent(interval=0.5))
+#
+# def get_ram_usage():
+#     print('RAM memory', psutil.virtual_memory())
+#     print('RAM memory total:', psutil.virtual_memory().total)
+#     print('RAM memory used:', psutil.virtual_memory().used,",",'RAM memory % used:', psutil.virtual_memory()[2])
+#     print('RAM memory available:', psutil.virtual_memory().available,",",'RAM memory % used:',100 - psutil.virtual_memory()[2])
+#
+# get_cpu_usage_per()
+# get_ram_usage()
 
-def get_cpu_usage_per():
-
-    print("CPU usage %",psutil.cpu_percent(interval=0.5))
-
-def get_ram_usage():
-    print('RAM memory', psutil.virtual_memory())
-    print('RAM memory total:', psutil.virtual_memory().total)
-    print('RAM memory used:', psutil.virtual_memory().used,",",'RAM memory % used:', psutil.virtual_memory()[2])
-    print('RAM memory available:', psutil.virtual_memory().available,",",'RAM memory % used:',100 - psutil.virtual_memory()[2])
-
-get_cpu_usage_per()
-get_ram_usage()
+print(platform.system())
--- nic.py
+++ nic.py
@@ -1,19 +1,46 @@
 import subprocess
-import re
+import platform
 
-cmd = "wmic nic get AdapterType, Name, Installed, MACAddress, GUID, Manufacturer, Availability, NetConnectionID, PowerManagementSupported, Speed."
-# cmd = "ipconfig"
-p1 = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-out, err = p1.communicate()
+if platform.system() == "Windows":
+    cmd = "wmic nic get AdapterType, Name, Installed, MACAddress, GUID, Manufacturer, Availability, NetConnectionID, PowerManagementSupported, Speed."
+    # cmd = "ipconfig"
+    p1 = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    out, err = p1.communicate()
 
-print('out: {0}'.format(out))
-print('error : {0}'.format(err))
+    print('out: {0}'.format(out))
+    print('error : {0}'.format(err))
 
-if p1.returncode == 0:
-    print('command : success')
+    if p1.returncode == 0:
+        print('command : success')
+    else:
+        print('command : failed')
+
+elif platform.system() == "Darwin":
+    cmd = "networksetup -listallhardwareports"
+
+    B = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    out, err = B.communicate()
+
+    if B.returncode == 0:
+        print('Result : Passed')
+    else:
+        print("Result : Failed")
+        print("err:couldn't find{0}".format(err))
+
+elif platform.system() == "Linux":
+    cmd = "hwinfo"
+
+    B = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    out, err = B.communicate()
+
+    if B.returncode == 0:
+        print('Result : Passed')
+    else:
+        print("Result : Failed")
+        print("err:couldn't find{0}".format(err))
+
 else:
-    print('command : failed')
-
+    print("OS not found")
 def Update_Version():
     Version_Number = '1.2'
 
